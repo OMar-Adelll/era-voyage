732c1fa5db797dde7977953294794a69249802c1
Nouraa0 <noura.reda0016@gmail.com>
2026-04-17 22:25:35 +0200

    js file

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+document.querySelector(".avatar").onclick = function() {
+    alert("I am the Guardian of Atlantis...");
+}
+
+
+document.querySelector(".card-1").onclick = () => {
+    document.getElementById("popup1").style.display = "flex";
+};
+
+document.querySelector(".card-2").onclick = () => {
+    document.getElementById("popup2").style.display = "flex";
+};
+
+document.querySelector(".card-3").onclick = () => {
+    document.getElementById("popup3").style.display = "flex";
+};
+
+document.querySelector(".card-4").onclick = () => {
+    document.getElementById("popup4").style.display = "flex";
+};
+
+
+document.querySelectorAll(".close").forEach(btn => {
+    btn.onclick = () => {
+        btn.closest(".popup").style.display = "none";
+    };
+});
